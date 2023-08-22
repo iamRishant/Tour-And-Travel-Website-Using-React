aa40d3712801cb2de2820212a48206327cf96811
Rishant Rana <kiturana378@gmail.com>
2023-08-22 09:45:46 +0530

    First Commit NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ScrollToTop from './components/ScrollToTop'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Recommended from './components/Recommended'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ScrollToTop/>
+      <Navbar/>
+      <Hero/>
+      <Services/>
+      <Recommended/>
+      <Testimonial/>
+      <Footer/>
+    </>
+  )
 }
-
-export default App;
