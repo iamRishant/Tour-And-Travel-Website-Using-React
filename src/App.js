db69b64a468f4fb5d39cd8bdd206e9360dda3029
Rishant Rana <kiturana378@gmail.com>
2023-08-28 22:46:49 +0530

    Added Animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import ScrollToTop from './components/ScrollToTop'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -6,8 +6,30 @@ import Services from './components/Services'
 import Recommended from './components/Recommended'
 import Testimonial from './components/Testimonial'
 import Footer from './components/Footer'
+import ScrollReveal from 'scrollreveal'
 
 export default function App() {
+  useEffect(()=>{
+    const sr= ScrollReveal({
+      origin:"top",
+      distance:"80px",
+      duration:2000,
+      reset:true,
+    });
+    sr.reveal(
+      `
+      nav,
+      #hero,
+      #services,
+      #recommend,
+      #testimonials,
+      footer
+      `,{
+        opacity:0,
+        interval:300,
+      }
+    )
+  },[])
   return (
     <>
       <ScrollToTop/>
